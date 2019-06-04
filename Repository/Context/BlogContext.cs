7440235256e59b30a9fc1ffec9dfd673388ba3fb
Vitor Yugulis <vitor.yugulis@gmail.com>
2019-06-04 16:22:11 -0300

    Change .env to root project folder

--- Repository/Context/BlogContext.cs
+++ Repository/Context/BlogContext.cs
@@ -12,7 +12,7 @@ namespace Repository.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            DotNetEnv.Env.Load("../Repository/.env", false);
+            DotNetEnv.Env.Load("../.env", false);
             var connString = DotNetEnv.Env.GetString("database");
             optionsBuilder.UseSqlite(connString);
         }
