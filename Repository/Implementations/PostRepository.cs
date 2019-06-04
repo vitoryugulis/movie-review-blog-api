0e14ee73f4334ec7c9c68633967f3580d75e1fa6
Vitor Yugulis <vitor.yugulis@gmail.com>
2019-06-04 03:56:14 -0300

    Override Get PostRepository methods to bring comments included

--- Repository/Implementations/PostRepository.cs
+++ Repository/Implementations/PostRepository.cs
@@ -1,11 +1,22 @@
 using System.Collections.Generic;
+using System.Linq;
 using Core.Interfaces.Repositories;
 using Core.Models;
+using Microsoft.EntityFrameworkCore;
 using Repository.Context;
 
 namespace Repository.Implementations
 {
     public class PostRepository : BaseRepository<Post>, IPostRepository
     {
+        public override IEnumerable<Post> GetAll()
+        {
+            return db.Posts.Include(x => x.Comments).ToList();
+        }
+
+        public override Post GetById(int id)
+        {
+            return db.Posts.Include(x => x.Comments).SingleOrDefault(x => x.Id == id);
+        }
     }
 }
